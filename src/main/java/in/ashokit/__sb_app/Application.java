08dad595e1f170c6a7e8ea24e9bc7e237faacf5a
ashokit <ashokitschool@gmail.com>
2025-10-25 13:11:37 +0530

    added variables

--- src/main/java/in/ashokit/__sb_app/Application.java
+++ src/main/java/in/ashokit/__sb_app/Application.java
@@ -7,7 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
+int i=10;
 		SpringApplication.run(Application.class, args);
+int j=20;
+
 	}
 
 }
