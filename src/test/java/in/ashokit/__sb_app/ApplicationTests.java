b46e1bb3ec7c5c590e3812f7d96a09f1af553039
ashokit <ashokitschool@gmail.com>
2025-10-25 12:25:04 +0530

    first commit

---
+++ src/test/java/in/ashokit/__sb_app/ApplicationTests.java
@@ -0,0 +1,13 @@
+package in.ashokit.__sb_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
